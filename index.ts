c720524f641020765e3bd1a18a85289600b0c414
Nick <moehrmann.n@icloud.com>
2023-10-05 15:23:41 +0200

    Added Server with API

--- index.ts
+++ index.ts
@@ -1,5 +1,18 @@
 import { Client, GatewayIntentBits } from "discord.js";
 import { Bot } from "./structs/Bot";
+import { Request, Response } from 'express';
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at ip http://localhost:${port}`);
+});
 
 export const bot = new Bot(
   new Client({
