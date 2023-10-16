3d9126169da4d910afa7a23df4489ed42aa718fd
--replace-all <nils.bohland@icloud.com>
2023-10-16 21:23:43 +0200

    docker set up
    Signed-off-by: --replace-all <nils.bohland@icloud.com>

---
+++ Database/schema.sql
@@ -0,0 +1,17 @@
+-- Create the Song table
+CREATE TABLE Song (
+                      id SERIAL PRIMARY KEY,
+                      name VARCHAR(255) NOT NULL,
+                      url VARCHAR(255) NOT NULL,
+                      author VARCHAR(255) NOT NULL,
+                      length INT NOT NULL
+);
+
+-- Create the Playlist table
+CREATE TABLE Playlist (
+                          id SERIAL PRIMARY KEY,
+                          name VARCHAR(255) NOT NULL,
+                          description TEXT,
+                          songs INT[] NOT NULL,
+                          FOREIGN KEY (songs) REFERENCES Song(id)
+);
