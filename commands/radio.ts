daf86dd81b526a3fed9a1fbb53b9bf0095daf7c1
Nils Bohland <bohland.nils.a22@student.dhbw-karlsruhe.de>
2023-10-16 15:29:40 +0200

    changed a single line for test purposes

--- commands/radio.ts
+++ commands/radio.ts
@@ -9,7 +9,6 @@ import YouTube from "youtube-sr";
 
 export default {
   data: new SlashCommandBuilder()
-
     .setName("radio")
     .setDescription(i18n.__("play.description"))
     .addStringOption((option) =>
