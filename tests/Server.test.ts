f47baacb5be431f85c46e2119a468aab26d0f2b2
Nick Möhrmann <81536709+Shiny1708@users.noreply.github.com>
2023-11-02 13:58:27 +0100

    Added new test pipeline (#73)
    * Added new test pipeline

--- tests/Server.test.ts
+++ tests/Server.test.ts
@@ -1,9 +1,26 @@
 import Server from "../structs/Server";
 
-describe("Test the API server", () => {
+describe("Hello Tests", () => {
   jest.useFakeTimers();
-  //const server = new Server(3000);
 
+  test("1 + 2 = 3", () => {
+    expect(1 + 2).toBe(3);
+  });
+
+  test('0 != "0"', () => {
+    expect(0).not.toBe("0");
+  });
+  /*
+      runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v3
+      - name: Use Node.js 20.9.0
+        uses: actions/setup-node@v3
+        with:
+          node-version: "20.9.0"
+          cache: "npm"
+      - run: npm ci
+      - run: npm run build --if-present
   test("empty string should result in zero", () => {
     let settings = {
       url: "http://localhost:3000/stop",
@@ -21,4 +38,6 @@ describe("Test the API server", () => {
       expect(response).toBe("");
     });
   });
+
+   */
 });
